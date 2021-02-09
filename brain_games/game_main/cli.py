ac2acc881b7504fe8418a038b7b3ece028a83a80
Andrey <OniSairento5@yandex.ru>
2021-02-09 11:43:41 +0700

    Fix flakes errors

--- brain_games/game_main/cli.py
+++ brain_games/game_main/cli.py
@@ -3,9 +3,6 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games!")
-<<<<<<< HEAD
     name = prompt.string("\n" "May I have your name? ")
-=======
     name = prompt.string("May I have you name? ")
->>>>>>> c2c6e176b2f6c77341f78123cea70cf1611dc644
     print("Hello,", name + "!")
