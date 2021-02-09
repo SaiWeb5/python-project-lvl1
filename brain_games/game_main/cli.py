43c65306881eed804f02ccf64904c46096244d95
Andrey <OniSairento5@yandex.ru>
2021-02-09 16:29:38 +0700

    Fix

--- brain_games/game_main/cli.py
+++ brain_games/game_main/cli.py
@@ -4,5 +4,4 @@ import prompt
 def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string("\n" "May I have your name? ")
-    name = prompt.string("May I have you name? ")
     print("Hello,", name + "!")
