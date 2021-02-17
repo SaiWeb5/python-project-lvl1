4593c0c46834b45005d3b3945e3006797a354891
Andrey <OniSairento5@yandex.ru>
2021-02-17 20:27:15 +0700

    Fix refactoring

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -5,25 +5,19 @@ def gcd_rules():
     return 'Find the greatest common divisor of given numbers.'
 
 
-def gen_num():
+def finding_the_divisor():
     num1 = random.randint(0, 100)
     num2 = random.randint(0, 100)
-    return num1, num2
-
-
-def finding_the_divisor():
-    num1, num2 = gen_num()
     while num1 != 0 and num2 != 0:  # Высчитываем общий делитель
         if num1 > num2:
             num1 %= num2
         else:
             num2 %= num1
     solution = num1 + num2
-    return solution
+    return num1, num2, solution
 
 
 def game_gcd():
-    num1, num2 = gen_num()
-    solution = finding_the_divisor()
+    num1, num2, solution = finding_the_divisor()
     task = '{} {}'.format(num1, num2)
     return task, str(solution)
